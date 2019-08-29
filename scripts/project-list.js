51397f27a826e45b40a41babc2845a62972fbd92
zakwarren <41547855+PoignantWizard@users.noreply.github.com>
2019-08-29 18:51:58 +0100

    Image paths

--- scripts/project-list.js
+++ scripts/project-list.js
@@ -3,7 +3,7 @@ const projects = [
         "id": "albieAdventures",
         "title": "Albie Adventures Game",
         "url": "https://www.sharemygame.com/share/484b2062-4881-47c1-bb65-efab8a04e49b",
-        "image": "images/albieadventures.png",
+        "image": "images/projects/albieadventures.png",
         "alt": "Albie Adventures Game",
         "blog": "blogs/albieAdventures.md"
     },
@@ -11,7 +11,7 @@ const projects = [
         "id": "cashlessCards",
         "title": "Cashless Cards System",
         "url": "https://github.com/zakwarren/cashlesscards",
-        "image": "images/cashlesscards.jpg",
+        "image": "images/projects/cashlesscards.jpg",
         "alt": "Cashless Cards System",
         "blog": "blogs/cashlessCards.md"
     },
@@ -19,7 +19,7 @@ const projects = [
         "id": "movieGenerator",
         "title": "Adam Sandler Movie Generator",
         "url": "https://twitter.com/AdamMovieGen",
-        "image": "images/moviegenerator.jpg",
+        "image": "images/projects/moviegenerator.jpg",
         "alt": "Adam Sandler Movie Generator",
         "blog": "blogs/movieGenerator.md"
     },
@@ -27,7 +27,7 @@ const projects = [
         "id": "activitiesClub",
         "title": "Activities Club Website",
         "url": "https://github.com/zakwarren/activitiesclub",
-        "image": "images/activitiesclub.jpg",
+        "image": "images/projects/activitiesclub.jpg",
         "alt": "Activities Club Website",
         "blog": "blogs/activitiesClub.md"
     },
