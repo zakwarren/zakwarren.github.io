4096a99d54d34a461a22045348af430dee96e04f
zakwarren <41547855+zakwarren@users.noreply.github.com>
2023-01-02 17:47:51 +0000

    Upgrade website

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,20 @@
 
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   site: "https://zakwarren.github.io/",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  }
 });
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface SkillItem {
+  description: string;
+  icon: string;
+  title: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
+
+export interface ShowcaseSite {
+  title: string;
+  image: ImageMetadata;
+  url: string;
+}
